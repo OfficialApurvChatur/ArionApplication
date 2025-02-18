a05ad9d239ce7371d20fa26b685deca34728eb5b
ApurvChatur <chaturapurv@gmail.com>
2025-02-18 21:45:57 +0530

    fix: backend url updated

--- site/admin/src/love/iSocket/socket.jsx
+++ site/admin/src/love/iSocket/socket.jsx
@@ -16,13 +16,13 @@ switch (import.meta.env.VITE_CORPORATION) {
 
                         switch (import.meta.env.VITE_APPLICATION) {
                             case 'ArionApplication':
-                                BASE_URL = "https://soup-arion-backend.onrender.com"
+                                BASE_URL = "https://backend-for-arion-application.onrender.com"
                                 break;
                             case 'AnkasApplication':
-                                BASE_URL = "https://soup-ankas-backend.onrender.com"
+                                BASE_URL = "https://backend-for-ankas-application.onrender.com"
                                 break;
                             case 'AbolApplication':
-                                BASE_URL = "https://soup-abol-backend.onrender.com"
+                                BASE_URL = "https://backend-for-abol-application.onrender.com"
                                 break;
                             default:
                                 break;
@@ -32,7 +32,7 @@ switch (import.meta.env.VITE_CORPORATION) {
                     case 'AnserEnterprise':
                         switch (import.meta.env.VITE_APPLICATION) {
                             case 'SwiftTalkApplication':
-                                BASE_URL = "https://soup-swift-talk-backend.onrender.com"
+                                BASE_URL = "https://backend-for-swift-talk-application.onrender.com"
                                 break;
                             default:
                                 break;
@@ -48,31 +48,31 @@ switch (import.meta.env.VITE_CORPORATION) {
 
                 switch (import.meta.env.VITE_APPLICATION) {
                     case 'BeehiveApplication':
-                        BASE_URL = "https://soup-beehive-backend.onrender.com"
+                        BASE_URL = "https://backend-for-beehive-application.onrender.com"
                         break;
                     case 'AndromedaApplication':
-                        BASE_URL = "https://soup-andromeda-backend.onrender.com"
+                        BASE_URL = "https://backend-for-andromeda-application.onrender.com"
                         break;
                     case 'PinwheelApplication':
-                        BASE_URL = "https://soup-pinwheel-backend.onrender.com"
+                        BASE_URL = "https://backend-for-pinwheel-application.onrender.com"
                         break;
                     case 'TadpoleApplication':
-                        BASE_URL = "https://soup-tadpole-backend.onrender.com"
+                        BASE_URL = "https://backend-for-tadpole-application.onrender.com"
                         break;
                     case 'AcruxApplication':
-                        BASE_URL = "https://soup-acrux-backend.onrender.com"
+                        BASE_URL = "https://backend-for-acrux-application.onrender.com"
                         break;
                     case 'AnserApplication':
-                        BASE_URL = "https://soup-anser-backend.onrender.com"
+                        BASE_URL = "https://backend-for-anser-application.onrender.com"
                         break;
                     case 'AquilaApplication':
-                        BASE_URL = "https://soup-aquila-backend.onrender.com"
+                        BASE_URL = "https://backend-for-aquila-application.onrender.com"
                         break;
                     case 'BellatrixApplication':
-                        BASE_URL = "https://soup-bellatrix-backend.onrender.com"
+                        BASE_URL = "https://backend-for-bellatrix-application.onrender.com"
                         break;
                     case 'CapellaApplication':
-                        BASE_URL = "https://soup-capella-backend.onrender.com"
+                        BASE_URL = "https://backend-for-capella-application.onrender.com"
                         break;
                     default:
                         break;
@@ -92,16 +92,16 @@ switch (import.meta.env.VITE_CORPORATION) {
 
                                 switch (import.meta.env.VITE_APPLICATION) {
                                     case 'TechfolioApplication':
-                                        BASE_URL = "https://soup-techfolio-backend.onrender.com"
+                                        BASE_URL = "https://backend-for-techfolio-application.onrender.com"
                                         break;
                                     case 'ApurvChaturApplication':
-                                        BASE_URL = "https://soup-apurv-chatur-backend.onrender.com"
+                                        BASE_URL = "https://backend-for-apurv-chatur-application.onrender.com"
                                         break;
                                     case 'AnushreeMandapeApplication':
-                                        BASE_URL = "https://soup-anushree-mandape-backend.onrender.com"
+                                        BASE_URL = "https://backend-for-anushree-mandape-application.onrender.com"
                                         break;
                                     case 'SofieBerkinApplication':
-                                        BASE_URL = "https://soup-sofie-berkin-backend.onrender.com"
+                                        BASE_URL = "https://backend-for-sofie-berkin-application.onrender.com"
                                         break;
                                     default:
                                         break;
@@ -112,16 +112,16 @@ switch (import.meta.env.VITE_CORPORATION) {
 
                                 switch (import.meta.env.VITE_APPLICATION) {
                                     case 'BlogifyApplication':
-                                        BASE_URL = "https://soup-blogify-backend.onrender.com"
+                                        BASE_URL = "https://backend-for-blogify-application.onrender.com"
                                         break;
                                     case 'SrimadBhagwatamApplication':
-                                        BASE_URL = "https://soup-srimad-bhagwatam-backend.onrender.com"
+                                        BASE_URL = "https://backend-for-srimad-bhagwatam-application.onrender.com"
                                         break;
                                     case 'BhagwadGitaApplication':
-                                        BASE_URL = "https://soup-bhagwad-gita-backend.onrender.com"
+                                        BASE_URL = "https://backend-for-bhagwad-gita-application.onrender.com"
                                         break;
                                     case 'InterviewQuestionApplication':
-                                        BASE_URL = "https://soup-interview-question-backend.onrender.com"
+                                        BASE_URL = "https://backend-for-interview-question-application.onrender.com"
                                         break;
                                     default:
                                         break;
@@ -143,7 +143,7 @@ switch (import.meta.env.VITE_CORPORATION) {
         break;
 
     default:
-        BASE_URL = "https://some-backend.onrender.com/SomeOtherStuff"
+        BASE_URL = "https://some-application.onrender.com/SomeOtherStuff"
         break;
 }
 
--- site/frontend/src/love/aAPI/APIBase.js
+++ site/frontend/src/love/aAPI/APIBase.js
@@ -6,7 +6,7 @@ const APIBase = (props) => {
     const configuration = {
         method: props.method,
         url: props.url,
-        baseURL: import.meta.env.VITE_ENVIRONMENT === "Production" ? "https://soup-arion-backend.onrender.com" : "http://localhost:8000",
+        baseURL: import.meta.env.VITE_ENVIRONMENT === "Production" ? "https://backend-for-arion-application.onrender.com" : "http://localhost:8000",
         headers: { 
             'Content-Type': 'application/json',
         },
