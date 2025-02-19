83310114afd31a56362c5361600a95c2a7b91b57
ApurvChatur <chaturapurv@gmail.com>
2025-02-19 17:29:25 +0530

    fix: backend url updated

--- site/admin/src/love/iSocket/socket.jsx
+++ site/admin/src/love/iSocket/socket.jsx
@@ -121,7 +121,7 @@ switch (import.meta.env.VITE_CORPORATION) {
                                         BASE_URL = "https://backend-for-bhagwad-gita-application.onrender.com"
                                         break;
                                     case 'InterviewQuestionApplication':
-                                        BASE_URL = "https://backend-for-interview-question-application.onrender.com"
+                                        BASE_URL = "https://backend-for-interview-question.onrender.com"
                                         break;
                                     default:
                                         break;
